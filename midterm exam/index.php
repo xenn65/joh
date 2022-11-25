9d430f12878c76803438f3454b66e3b63865c8b8
xenn65 <114554488+xenn65@users.noreply.github.com>
2022-11-25 16:32:12 +0800

    Add files via upload

---
+++ midterm exam/index.php
@@ -0,0 +1,92 @@
+<?php
+// john paul Azurin
+// BSIT 4
+
+// <!-- VAriable -->
+
+ $x = 2021; 
+$y = 2022; 
+
+
+// <!-- Echo -->
+
+echo "Welcome john paul!";
+
+
+
+// <!-- data type -->
+
+// <!-- string -->
+
+$string = "2022";
+
+if ( is_numeric($string) ) {
+    echo "\"{$string}\" is a number.";
+} else {
+    echo "\"{$string}\" is not a number.";
+}
+
+// <!-- numbers -->
+
+// <!-- math -->
+
+echo " 4+4" ;
+echo <br>
+echo " 4%4" ;
+echo <br>
+echo " 4/4" ;
+echo <br>
+echo " 4-4" ;
+echo <br>
+echo " 4*4" ;
+echo <br>
+
+// <!-- constants -->
+
+
+// <!-- Operators -->
+
+ echo " 4+4" ;
+ echo <br>
+ echo " 4%4" ;
+ echo <br>
+ echo " 4/4" ;
+ echo <br>
+ echo " 4-4" ;
+ echo <br>
+ echo " 4*4" ;
+ echo <br>
+
+//  if..else.. if
+
+if ($t < "10") {
+    echo "Good Morning!";
+  } elseif ($t < "20") {
+    echo "Have a Nice day!";
+  } else {
+    echo "Have Goodbye:)!";
+  }
+//   switch
+
+$favDessert = "IceCream";
+
+switch ($favDessert) {
+  case "Apple":
+    echo "A";
+    break;
+  case "Banana":
+    echo " B";
+    break;
+  case "Cake":
+    echo "C";
+    break;
+ 
+}
+
+// loops
+
+for ( $ , $=1 )
+
+// functoion
+
+  ?>
